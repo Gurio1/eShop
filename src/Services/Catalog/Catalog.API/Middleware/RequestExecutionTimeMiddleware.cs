f3a12005f59e4c0beaea6be7c8c8db985858f11e
Andrey Iliev <iliev.andreyy@gmail.com>
2023-03-14 23:04:40 +0200

    Add test middleware

---
+++ src/Services/Catalog/Catalog.API/Middleware/RequestExecutionTimeMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Catalog.API.Middleware;
+
+public class RequestExecutionTimeMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RequestExecutionTimeMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext,ILogger<RequestExecutionTimeMiddleware> logger)
+    {
+        var startTime = Stopwatch.GetTimestamp();
+        
+        await _next(httpContext);
+        
+        logger.LogInformation(1,$"Request with path {httpContext.Request.Path} was executed for {Stopwatch.GetElapsedTime(startTime).TotalMilliseconds} ms");
+    }
+}
+
+public static class RequestExecutionTimeMiddlewareExtension
+{
+    public static IApplicationBuilder UseRequestExecutionTimeMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<RequestExecutionTimeMiddleware>();
+    }
+}
