d44d72316f34388bf8c791dc4fb43708614714ea
Andrey Iliev <iliev.andreyy@gmail.com>
2023-03-22 00:31:40 +0200

    Add new viewModel for CatalogItem

---
+++ src/Services/Catalog/Catalog.API/ViewModels/ItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.ViewModels;
+
+public class ItemViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string PictureFileName { get; set; } = null!;
+
+    public int CatalogItemTypeId { get; set; }
+
+    public int CatalogItemBrandId { get; set; }
+}
