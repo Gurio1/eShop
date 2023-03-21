85c09f5b654addfb55977a2841fa1b9bbcd2431e
Andrey Iliev <iliev.andreyy@gmail.com>
2023-03-22 00:32:23 +0200

    Create profile for mappings for CatalogItem

---
+++ src/Services/Catalog/Catalog.API/Profiles/CatalogItemProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Catalog.API.Models;
+using Catalog.API.ViewModels;
+
+namespace Catalog.API.Profiles;
+
+public class CatalogItemProfile : Profile
+{
+    public CatalogItemProfile()
+    {
+        CreateMap<CatalogItem, ItemViewModel>();
+    }
+}
