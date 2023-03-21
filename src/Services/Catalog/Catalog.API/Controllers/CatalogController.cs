f51a4d2347d0891d10910569f3e9465153a85bbc
Andrey Iliev <iliev.andreyy@gmail.com>
2023-03-22 00:31:05 +0200

    Add new endpoint and update existing one

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using AutoMapper;
 using Catalog.API.Models;
 using Catalog.API.Persistence;
 using Catalog.API.ViewModels;
@@ -7,23 +8,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.API.Controllers;
 
-[Route("[controller]")]
+[Route("api/v1/[controller]")]
 [ApiController]
 public class CatalogController:ControllerBase
 {
     private readonly ILogger<CatalogController> _logger;
     private readonly CatalogContext _catalogContext;
+    private readonly IMapper _mapper;
 
-    public CatalogController(ILogger<CatalogController> logger,CatalogContext catalogContext)
+    public CatalogController(ILogger<CatalogController> logger,CatalogContext catalogContext,IMapper mapper)
     {
         _logger = logger;
         _catalogContext = catalogContext;
+        _mapper = mapper;
     }
     [HttpGet]
     [Route("items")]
     [ProducesResponseType((int)HttpStatusCode.BadRequest)]
     [ProducesResponseType(typeof(PaginatedItems<CatalogItem>),(int)HttpStatusCode.OK)]
-    public async Task<IActionResult> ItemsAsync([FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 0)
+    public async Task<IActionResult> ItemsAsync([FromServices] IWebHostEnvironment env,[FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 0)
     {
         var totalItems = await _catalogContext.CatalogItems.LongCountAsync();
 
@@ -33,10 +36,32 @@ public class CatalogController:ControllerBase
             .Take(pageSize)
             .ToArrayAsync();
 
-        //Need to decide what to do with ImageUrl
-        //Pic controller(working not correct) is one of the solutions,but i need time
-        
+        foreach (var item in itemsOnPage)
+        {
+            item.PictureUri = $"http://localhost:5178/images/{item.PictureFileName}";
+        }
+
         var viewModel = new PaginatedItems<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
         return Ok(viewModel);
+    }
+
+    [HttpGet]
+    [Route("item/{id}")]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        return null;
+    }
+    
+    [HttpPost]
+    [Route("items")]
+    [ProducesResponseType((int)HttpStatusCode.Created)]
+    public async Task<IActionResult> CreateItemAsync([FromBody]ItemViewModel item)
+    {
+        var product = _mapper.Map<CatalogItem>(item);
+
+        await _catalogContext.CatalogItems.AddAsync(product);
+        await _catalogContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetByIdAsync), new { id = product.Id }, null);
     }
 }
